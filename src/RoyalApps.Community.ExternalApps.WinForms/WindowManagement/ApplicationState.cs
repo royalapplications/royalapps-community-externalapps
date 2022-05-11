36c308237c633a6422d84f66404332ed18b3abd5
Hans Docsek <hans.docsek@gmail.com>
2022-05-11 11:19:50 +0200

    add comments

--- src/RoyalApps.Community.ExternalApps.WinForms/WindowManagement/ApplicationState.cs
+++ src/RoyalApps.Community.ExternalApps.WinForms/WindowManagement/ApplicationState.cs
@@ -1,8 +1,13 @@
-namespace RoyalApps.Community.ExternalApps.WinForms;
+namespace RoyalApps.Community.ExternalApps.WinForms.WindowManagement;
 
+/// <summary>
+/// Reflects the current state of an external application.  
+/// </summary>
 public enum ApplicationState
 {
+#pragma warning disable CS1591
     Starting,
     Running,
     Stopped
+#pragma warning restore CS1591    
 }
